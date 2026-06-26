689a6836549577076e84f46946ee31985aac52ac
Alan Yeung <alanyeung99@outlook.com>
2026-06-26 15:12:40 -0700

    Fix deploy issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
+    build: {
+      outDir: 'dist',
+      sourcemap: false
+    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3000',
+    //     changeOrigin: true
+    //   }
+    // }
   }
 });
